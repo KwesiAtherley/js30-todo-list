cfa9cd1e27f06a54eb5c0e80bcbe9936bb348076
kwesiatherley <kwesiatherley@gmail.com>
2019-05-31 12:45:35 -0400

    Updates

---
+++ precache-manifest.10426cafe8ee73a2a88468940d67fd82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d64331ff253114cb00a3da89edf7b4",
+    "url": "/js30-todo-list/index.html"
+  },
+  {
+    "revision": "438b47964f80db375580",
+    "url": "/js30-todo-list/static/css/main.79074318.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/js30-todo-list/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "438b47964f80db375580",
+    "url": "/js30-todo-list/static/js/main.ac1ca062.chunk.js"
+  },
+  {
+    "revision": "1c6d34a12f9a3eac52c2",
+    "url": "/js30-todo-list/static/js/runtime~main.0d8cb116.js"
+  }
+]);
